0f4c250c062c81be51c39d622b3b6d020adab03b
Lubni Morais <lubni.morais@gmail.com>
2024-03-22 22:14:07 -0300

    feat: add Flatlist in GroupsScreen

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,17 +1,26 @@
+import { useState } from 'react';
+
 import { Header } from '@components/Header';
 
 import { GroupsContainer } from './styles';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { FlatList } from 'react-native';
 
 export function GroupsScreen() {
+  const [groups, setGroups] = useState<string[]>(['Galera da Rocketseat']);
+
   return (
     <GroupsContainer>
       <Header />
 
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
 
-      <GroupCard title="Usuários" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item: group }) => <GroupCard title={group} />}
+      />
     </GroupsContainer>
   );
 }
